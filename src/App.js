8954c580d35f8714d57b1c9686803a6ebbf575ba
Rodrigo Pedraza <rpedraza01@gmail.com>
2023-01-26 15:49:01 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [count, setCount] = React.useState(0);
+  const [error, setError] = React.useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div data-test="component-app">
+      <h1 data-test="counter-display">
+        The counter is currently&nbsp;
+        <span data-test="count">{count}</span>
+      </h1>
+      <button 
+        data-test="increment-button"
+        onClick={() => setCount(count + 1)}>Increment counter
+      </button>
+      <button
+        data-test="decrement-button"
+        onClick={() => {
+          if (count > 0) {
+            setCount(count - 1)
+          } else {
+            setError(true);
+          }
+        }
+      }
+      >Decrement counter
+      </button>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,68 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
 import App from './App';
+import Enzyme, { shallow, ShallowWrapper } from 'enzyme';
+import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// setup enzyme's react adapter
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+/**
+  * Factory function to create a ShalloWrapper for the App component
+  * @function setup
+  * @returns {ShallowWrapper}
+ */
+const setup = () => shallow(<App />);
+
+const findByTestAttr = (wrapper, val) => wrapper.find(`[data-test='${val}']`);
+
+test('renders without error', () => {
+  const wrapper = setup();
+  const appComponent = findByTestAttr(wrapper, "component-app");
+  expect(appComponent.length).toBe(1);
+});
+
+test('renders increment button', () => {
+  const wrapper = setup();
+  const button = findByTestAttr(wrapper, "increment-button");
+  expect(button.length).toBe(1);
+});
+
+test('renders counter display', () => {
+  const wrapper = setup();
+  const counterDisplay = findByTestAttr(wrapper, "counter-display");
+  expect(counterDisplay.length).toBe(1);
+});
+
+test('counter starts at 0', () => {
+  const wrapper = setup();
+  const count = findByTestAttr(wrapper, "count").text();
+  expect(count).toBe("0");
+});
+
+test('clicking button increments counter display', () => {
+  const wrapper = setup();
+
+  // find the button
+  const button = findByTestAttr(wrapper, "increment-button");
+
+  // click the button
+  button.simulate('click');
+
+  // find the display, and test that the number has been incremented
+  const count = findByTestAttr(wrapper, "count").text();
+  expect(count).toBe("1");
+});
+
+test('clicking second button decrements counter display', () => {
+  const wrapper = setup();
+
+  // find the decrement button
+  const button = findByTestAttr(wrapper, "decrement-button");
+
+  // click the decrement button
+  button.simulate('click');
+
+  // find the display, and test that the number has been decremented
+  const count = findByTestAttr(wrapper, "count").text();
+  expect(count).toBe("-1");
 });
